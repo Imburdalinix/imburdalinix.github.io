5d0d12680a7a0502bb2c9dc514440fb863808dae
Panda5598 <85232768+Imburdalinix@users.noreply.github.com>
2024-05-17 15:32:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function updateTime() {
     const options = { timeZone: 'Asia/Kolkata', hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' };
-    document.getElementById('clock-mumbai').textContent = 'Mumbai: ' + new Intl.DateTimeFormat('en-GB', options).format(new Date());
+    document.getElementById('clock-mumbai').textContent = new Intl.DateTimeFormat('en-GB', options).format(new Date());
 
     options.timeZone = 'Asia/Shanghai';
-    document.getElementById('clock-shanghai').textContent = 'Shanghai: ' + new Intl.DateTimeFormat('en-GB', options).format(new Date());
+    document.getElementById('clock-shanghai').textContent = new Intl.DateTimeFormat('en-GB', options).format(new Date());
 
     options.timeZone = 'Europe/Zurich';
-    document.getElementById('clock-frauenfeld').textContent = 'Frauenfeld: ' + new Intl.DateTimeFormat('en-GB', options).format(new Date());
+    document.getElementById('clock-frauenfeld').textContent = new Intl.DateTimeFormat('en-GB', options).format(new Date());
 
     options.timeZone = 'America/New_York';
-    document.getElementById('clock-southington').textContent = 'Southington: ' + new Intl.DateTimeFormat('en-GB', options).format(new Date());
+    document.getElementById('clock-southington').textContent = new Intl.DateTimeFormat('en-GB', options).format(new Date());
 }
 
 setInterval(updateTime, 1000);
